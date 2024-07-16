261c2dc63f053bc0376ee87c090b3bfb2d4ed031
Tallone <tallone.shi@outlook.com>
2024-07-16 14:37:22 +0800

    Opt neovide curser particles

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,4 +5,5 @@ if vim.g.neovide then
   vim.o.guifont = "JetBrainsMonoNL Nerd Font Mono,LXGW WenKai Mono GB:h14"
   vim.g.neovide_transparency = 0.95
   vim.g.neovide_floating_shadow = true
+  vim.g.neovide_cursor_vfx_mode = "railgun"
 end
